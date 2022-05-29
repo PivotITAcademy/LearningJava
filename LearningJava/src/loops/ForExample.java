afd31d297b5629c50018f3a69f88734af419882e
ketakipillay <ketakipillay@gmail.com>
2022-05-29 12:29:29 -0400

    Loops

---
+++ LearningJava/src/loops/ForExample.java
@@ -0,0 +1,78 @@
+package loops;
+
+import java.util.Scanner;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * for(initialize;condition;increment/decrement)
+		 */
+
+		// Print number in reverse order
+//		for(int num=10;num>=1;num--) {
+//			System.out.println(num);
+//		}
+
+		// Print 2, 4, 6, 8 , 10
+//		for(
+//				int num=2;
+//				num<=10;
+//				num+=2) {
+//			System.out.println(num);
+//			
+//		}
+
+		// Print table of 3
+		
+//
+//		for (int num = 3; num <= 30; num += 3) {
+//			System.out.println(num);
+//		}
+
+		// Print table of 3
+		// 3X1 =3
+		// 3X2 =6
+		// 3X3 =9
+		// 3X4=12
+//		for(int num=1;num<=10;num++) {
+//			System.out.println("3X"+num+"="+num*3);
+//		}
+		
+		//Number can go upto 100, print all the numbers from 1 to 100 which are divisible by both 3 & 5
+		
+		
+//		for(int num =1;num<=100;num++) {
+//			if(num%3==0 && num%5==0) {
+//				System.out.println(num);
+//			}
+//		}
+		
+		// Print table of number where num can be dynamic 
+		
+//		System.out.println("Enter a number for which u want to print the table");
+//		
+		Scanner sc = new Scanner(System.in);
+//		
+//		int number = sc.nextInt();
+//		
+//		for(int i=1;i<=10;i++) {
+//			System.out.println(number+"X"+i+"="+number*i);
+//		}
+		
+//		//There is a university, If n of students join us, print welcome n numbers of times
+//		
+//		System.out.println("enter the number of students");
+//		
+//		int num = sc.nextInt();
+//		
+//		for(int i=1;i<=num;i++) {
+//			System.out.println("Welcome");
+//		}
+		
+		
+	}
+
+}
